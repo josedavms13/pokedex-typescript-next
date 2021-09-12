c656eb07ad23e4792c975813713440e7bbe61251
josedavms13 <josedav0ms13@gmail.com>
2021-09-11 23:16:52 -0500

    Feat: -> Dispatch user name

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,15 +1,72 @@
-import React, {useEffect} from 'react';
-import {useSelector} from "react-redux";
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from '../customTypes/reduxTypes'
+import {setUser} from '../state/'
 
 function Login() {
-    // @ts-ignore
-    const language = useSelector(state => state.language);
+
+
+    //Redux
+    const language = useSelector((state:RootState) => state.language);
+    const dispatch = useDispatch();
+
+
+    //region Language management
+    interface languageObject{
+        label: string,
+        submitButton: string,
+    }
     useEffect(()=>{
         console.log(language)
+        switch(language){
+            case 'english' :
+                setLabelTxt({
+                    label: 'Please type your name',
+                    submitButton : 'Submit'
+                })
+                break
+
+            case 'spanish':
+                setLabelTxt({
+                    label: 'Porfavor ingresa tu nombre',
+                    submitButton : 'Enviar'
+                })
+        }
+
     },[language])
+
+
+    const [labelTxt, setLabelTxt] = useState<languageObject>({
+        label: 'Please type your name',
+        submitButton : 'Submit'
+    });
+    //endregion Language management
+
+
+
+    //region Submit button actions
+
+    // Get data from user input
+    const [userInput, setUserInput] = useState<string>('');
+    const [disableSubmitButton, setDisableSubmitButton] = useState<boolean>(true);
+    useEffect(()=>{
+        userInput.length > 3? setDisableSubmitButton(false) : setDisableSubmitButton(true)
+    },[userInput])
+
+
+    function submitName():void{
+
+        dispatch(setUser(userInput));
+    }
+
+
+    //endregion Submit button actions
+
     return (
         <div>
-            {language}
+            <h2>{labelTxt.label}</h2>
+            <input onChange={(event => setUserInput(event.target.value))} type="text"/>
+            <button onClick={submitName} disabled={disableSubmitButton}>{labelTxt.submitButton}</button>
         </div>
     );
 }
