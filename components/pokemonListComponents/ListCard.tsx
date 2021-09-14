cea1c17bf5304082a779717427d0b30fe902422b
josedavms13 <josedav0ms13@gmail.com>
2021-09-13 22:17:58 -0500

    FEAT: -> Datavalues shown on listCard error: -> Pagination works bad

--- components/pokemonListComponents/ListCard.tsx
+++ components/pokemonListComponents/ListCard.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 import React, {useEffect, useState} from 'react';
 import {getSpecificPokemon} from '../../services/pokemons.get'
 import {firstLetterCapitalize} from "../../utils/firstLetterCapitalize";
-
+import {useSelector} from "react-redux";
+import {RootState} from "../../customTypes/reduxTypes";
+import {decimetersToMeters, hectogramsToKilograms} from '../../utils/unitsConversion'
 
 interface pokemonCardList{
     pokemonUrl : string,
@@ -14,17 +16,17 @@ function ListCard({name, pokemonUrl}:pokemonCardList) {
     const [pokemonFetchResult, setPokemonFetchResult] = useState<any>(null);
     const [pokemonImages, setPokemonImages] = useState<string[]>(['']);
     const [currentImage, setCurrentImage] = useState<string>('');
-    console.log(name);
-    console.log(pokemonUrl);
+
+    //Get particular pokemon info
     useEffect(()=>{
         getSpecificPokemon(pokemonUrl)
             .then((data:any) => setPokemonFetchResult(data))
             .catch(error => console.log(error))
     },[pokemonUrl])
 
+    // Filter sprites / Removing Null values and object types
     useEffect(() => {
-        console.log(pokemonFetchResult);
-
+        console.log(pokemonFetchResult)
         if (pokemonFetchResult) {
             // @ts-ignore
             setPokemonImages(Object.values(pokemonFetchResult.data.sprites).filter((element: any)=> {
@@ -34,18 +36,71 @@ function ListCard({name, pokemonUrl}:pokemonCardList) {
         
     }, [pokemonFetchResult]);
 
+
+    // Initialize default sprite to show
     useEffect(() => {
-        console.log(pokemonImages)
         if(pokemonImages) setCurrentImage(pokemonImages[0]);
     }, [pokemonImages]);
 
 
 
-    function alternateImages(){
-        const imageIndex = Math.floor(Math.random() * pokemonImages.length)
-
-        setCurrentImage(pokemonImages[imageIndex]);
+    function alternateImages():number{
+        return Math.floor(Math.random() * pokemonImages.length)
     }
+
+
+
+    //region Language management
+
+
+    const language = useSelector((state:RootState)=>state.language);
+    const [labelText, setLabelText] = useState<any>({
+        typeTitle: 'Main Type',
+        physicCharacteristics: {
+            tittle:'Physical Characteristics',
+            weight : 'Weight',
+            height: 'Height'
+        }
+    });
+    useEffect(()=>{
+
+        switch (language) {
+            case 'spanish':
+
+                setLabelText({
+                    ...labelText,
+                    typeTitle: 'Tipo principal',
+                    physicCharacteristics: {
+                        tittle:'Características físicas',
+                        weight : 'Peso',
+                        height: 'Altura'
+                    }
+                })
+                break
+            case 'english':
+
+                setLabelText({
+                    ...labelText,
+                    typeTitle: 'Main Type',
+                    physicCharacteristics: {
+                        tittle:'Physical Characteristics',
+                        weight : 'Weight',
+                        height: 'Height'
+                    }
+                })
+                break
+
+
+        }
+
+    },[language, labelText])
+
+
+
+    //endregion Language management
+
+
+
 
 
     return (
@@ -62,10 +117,21 @@ function ListCard({name, pokemonUrl}:pokemonCardList) {
                 </div>
 
                 <div className="characteristics">
+                    <div className="types">
+                        <h6>{labelText.typeTitle}</h6>
+                        {firstLetterCapitalize(pokemonFetchResult.data.types[0].type.name)}
+                    </div>
+
+                    <div className="physic-characteristics">
+                        <h6>{labelText.physicCharacteristics.title}</h6>
+                        <span><b>{labelText.physicCharacteristics.height}:</b> {decimetersToMeters(pokemonFetchResult.data.height)} m</span>
+                        <span><b>{labelText.physicCharacteristics.weight}:</b> {hectogramsToKilograms(pokemonFetchResult.data.weight)} Kg</span>
+
+                    </div>
 
                 </div>
                 
-                <div onClick={alternateImages} className="image">
+                <div onClick={()=>{setCurrentImage(pokemonImages[alternateImages()])}} className="image">
                     <img src={currentImage} alt={name}/>
                 </div>
 
---
+++ utils/unitsConversion.ts
@@ -0,0 +1,13 @@
+
+
+export function decimetersToMeters(decimeters:number):number{
+
+
+    return Number((decimeters / 10).toFixed(2))
+}
+
+
+export function hectogramsToKilograms(hectograms:number):number{
+
+    return Number((hectograms / 10).toFixed(2))
+}
